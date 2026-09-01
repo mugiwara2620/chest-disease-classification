8d6a94bc2a46a825a28e9c0775a2195e021df25a
John Doe <ameaymaneelabba@gmail.com>
2026-09-01 04:01:06 +0100

    fix the transformation

--- src/chest_disease/config/configuration.py
+++ src/chest_disease/config/configuration.py
@@ -12,15 +12,17 @@ class ConfigurationManager:
         self.params = read_yaml(params_filepath)
         create_directories([self.config.artifacts_root])
 
-    def get_data_ingestion_config(self):
+    def get_data_ingestion_config(self) -> DataIngestionConfig:
         config = self.config.data_ingestion
+
         create_directories([config.root_dir])
 
         data_ingestion_config = DataIngestionConfig(
             root_dir=Path(config.root_dir),
-            source_URL=config.source_URL,
-            local_data_file=Path(config.local_data_file),
             unzip_dir=Path(config.unzip_dir),
+            workspace=config.workspace,
+            project=config.project,
+            version=int(config.version),
         )
 
         return data_ingestion_config
--- src/chest_disease/entity/config_entity.py
+++ src/chest_disease/entity/config_entity.py
@@ -5,9 +5,10 @@ from pathlib import Path
 @dataclass(frozen=True)
 class DataIngestionConfig:
     root_dir: Path
-    source_URL: str
-    local_data_file: Path
     unzip_dir: Path
+    workspace: str
+    project: str
+    version: int
 
 
 @dataclass(frozen=True)
